4ae6b8f7fcdfd19ee49faf29cca9217b128d7ad3
Manni <manuelkenna@gmail.com>
2016-08-30 11:04:46 -0500

    test

--- src/main/java/com/findme/controller/AdminController.java
+++ src/main/java/com/findme/controller/AdminController.java
@@ -2,14 +2,20 @@ package com.findme.controller;
 
 
 
+import javax.servlet.http.HttpSession;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.findme.service.ProfessionalService;
+import com.findme.service.UserAccountService;
 
 @Controller("adminController")
 @RequestMapping("/admin")
@@ -18,6 +24,9 @@ public class AdminController {
 
 	@Autowired
 	private ProfessionalService professionalService;
+	
+	@Autowired
+	private UserAccountService userAccountService;
 	
 	@RequestMapping(value="/professionals/{professionalId}/{isActive}", method=RequestMethod.GET)
 	public String updateProfessionalStatus(@PathVariable("professionalId") Long professionalId, 
@@ -29,5 +38,14 @@ public class AdminController {
 		
 		return "redirect:/admin";
 	}
+	
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String homePage(ModelMap model, HttpSession session) {
+		String username = ((User)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+		com.findme.domain.User user = userAccountService.findUserByUsername(username);
+		session.setAttribute("loggedUser", user);
+		model.addAttribute("professionals",professionalService.findAll());
+        return "home";
+    }
 	
 }
